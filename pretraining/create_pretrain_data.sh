ba12ef04d5ab9cb71ade1624fcf31049bdac70d2
Petr Zelina <469366@mail.muni.cz>
2020-05-22 00:21:04 +0200

    prettify pretrain data creation script

--- pretraining/create_pretrain_data.sh
+++ pretraining/create_pretrain_data.sh
@@ -1,18 +1,23 @@
+# script for running pretraining data creation
 
-base="albert"
-#infiles="datasets/tenten/preproc_sentences/tenten_proc_101.txt,datasets/tenten/preproc_sentences/tenten_proc_102.txt"
-#infiles="datasets/tenten/preproc_sentences/test1.txt"
-bn=`basename "$1"`
-outdir="datasets/tta512"
+# usage: ./create_pretrain_data.sh corpus.txt
 
+# multiple in parallel:
+# find datasets/preproc/tenten/*.txt | parallel --ungroup -j 6 ./create_pretrain_data.sh {}
 
-python3 $base/create_pretraining_data.py \
+outdir="datasets/tta256"           # output directory
+spm="../corpus/spm/spm_tta_30K"    # SentencePiece model
+
+infile="$1"                        # input corpus txt file
+bn=`basename "$infile"`
+
+python3 albert/create_pretraining_data.py \
 	--input_file "$1" \
 	--output_file "$outdir/$bn.pre" \
-	--vocab_file ../corpus/spm/spm_tta_30K.vocab \
-	--spm_model_file ../corpus/spm/spm_tta_30K.model \
-	--max_seq_length 512 \
 	--meta_data_file_path "$bn.meta" \
+	--vocab_file "$spm.vocab" \
+	--spm_model_file "$spm.model" \
+	--max_seq_length 256 \
 	--do_lower_case=True \
 	--dupe_factor 1 \
 	--do_whole_word_mask=False
